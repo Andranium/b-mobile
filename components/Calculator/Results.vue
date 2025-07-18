63bdb3d7f224d533e4bdb0c7ed0f2ca035d1b5c8
Andranium <andranik_galstyan@bk.ru>
2025-07-18 16:41:02 +0300

    small change

--- components/Calculator/Results.vue
+++ components/Calculator/Results.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="calculator-results">
-    <div class="calculator-results__text">
-    </div>
+    <div class="calculator-results__text"/>
 
     <div class="calculator-results__buttons">
-      <UButton size="xl" block class="w-48 py-5 text-lg rounded-xl" color="primary">
-        Рассчитать
-      </UButton>
+<!--      <UButton-->
+<!--          v-if="!calc"-->
+<!--          :disabled="isButtonDisabled"-->
+<!--          size="xl"-->
+<!--          block-->
+<!--          class="w-48 py-5 text-lg rounded-xl"-->
+<!--          color="primary"-->
+<!--          @click="calculate"-->
+<!--      >-->
+<!--        Рассчитать-->
+<!--      </UButton>-->
+
+<!--      <template v-else>-->
+<!--        <UButton-->
+<!--            :disabled="isButtonDisabled"-->
+<!--            size="xl"-->
+<!--            block-->
+<!--            class="w-48 py-5 text-lg rounded-xl"-->
+<!--            color="primary"-->
+<!--            @click="calculate"-->
+<!--        >-->
+<!--          Рассчитать-->
+<!--        </UButton>-->
+
+<!--        <UButton-->
+<!--            :disabled="isButtonDisabled"-->
+<!--            size="xl"-->
+<!--            block-->
+<!--            class="w-48 py-5 text-lg rounded-xl"-->
+<!--            color="primary"-->
+<!--            @click="calculate"-->
+<!--        >-->
+<!--          Рассчитать-->
+<!--        </UButton>-->
+<!--      </template>-->
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {useCalculatorStore} from "~/store/calculator/useCalculatorStore";
 
+const calculatorStore = useCalculatorStore();
+
+const calc = ref(false); // temporary
+
+const isButtonDisabled  = computed(() => {
+  return !(calculatorStore.date && calculatorStore.car);
+});
+
+const calculate = () => {
+  calc.value = true;
+}
 </script>
 
 <style scoped lang="scss">
@@ -21,5 +64,10 @@
   align-items: center;
   justify-content: space-between;
   width: 100%;
+
+  &__buttons {
+    display: flex;
+    gap: 8px;
+  }
 }
 </style>
